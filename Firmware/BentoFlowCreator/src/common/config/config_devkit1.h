32fc7d80c5ae08e237eff2e6e02d3395a8ad00c1
luisgongod <luis.gongod@gmail.com>
2020-07-29 21:45:30 +0200

    ♻️ reformat and refactor code to show

--- Firmware/BentoFlowCreator/src/common/config/config_devkit1.h
+++ Firmware/BentoFlowCreator/src/common/config/config_devkit1.h
@@ -2,7 +2,6 @@
 
 // Flowtoys Creator Club
 #define DEVICE_TYPE "Flowtoys Creator Club"
-// #define DEVICE_TYPE "M5-proto"
 
 #ifndef ESP32
 #define ESP32
@@ -15,43 +14,10 @@
 #define LED_COLOR_ORDER GRB
 // #define LED_INVERT_DIRECTION
 
-//main power
-// #define KEEP_SLEEP_PIN_HIGH
-// #define SLEEP_PIN 12 // pin for turning the LDO on and off. LOW for off and HIGH for on. 
-// #define SLEEP_PIN_SLEEP_VAL LOW
-// #define SLEEP_WAKEUP_BUTTON GPIO_NUM_32
-// #define SLEEP_WAKEUP_STATE HIGH
-
-//setting low will colapse the power and the club will only turn on from USB or button press.
-
 // button pin
 #define BUTTON_COUNT 1
 #define BUTTON_INPUT_MODE INPUT
 const int buttonPins[BUTTON_COUNT]{ 2 };
-
-//battery sence
-// #define BATTERY_PIN 35 // takes the measurment from the battery sence. 
-
-// #define HAS_IMU
-//#define I2C_CLOCK 10000 // not sure what this is for.
-// #define SDA_PIN 23 // i2c lines
-// #define SCL_PIN 22 // i2c lines
-// #define IMU_REMAP_CONFIG Adafruit_BNO055::REMAP_CONFIG_P0
-// #define IMU_REMAP_SIGN Adafruit_BNO055::REMAP_SIGN_P0
-
-// #define INT_PIN 33 // the interupt pin for the IMU MPU prosessor.
-// #define IMU_RESET 21 // toggl this pin to reset the IMU before connectiong to it after a reset.
-// #define IMU_ADDR 0x28
-// #define BNO055_SAMPLERATE_DELAY_MS (100)
-
-// ir config
-// #define HAS_IR
-
-// #define IR_TX_PIN 17 // drive with PWM HIGH is on and LOW is off.
-// #define IR_MAX_POWER 25 // 10%
-// #define IR_CHANNEL 0
-// #define IR_PWM_RESOLUTION 8 //8 = value range from 0 to 255
-// #define IR_FREQ 5000
 
 // SD config
 #define HAS_FILES
--- Firmware/BentoFlowCreator/src/common/config/config_m5stick.h
+++ Firmware/BentoFlowCreator/src/common/config/config_m5stick.h
@@ -3,7 +3,6 @@
 // #define DEVICE_TYPE "M5Stick"
 #define DEVICE_TYPE "Flowtoys Creator Club"
 
-
 #ifndef ESP32
 #define ESP32
 #endif
@@ -15,51 +14,10 @@
 #define LED_COLOR_ORDER GRB
 // #define LED_INVERT_DIRECTION
 
-//main power
-// #define KEEP_SLEEP_PIN_HIGH
-// #define SLEEP_PIN 12 // pin for turning the LDO on and off. LOW for off and HIGH for on. 
-// #define SLEEP_PIN_SLEEP_VAL LOW
-// #define SLEEP_WAKEUP_BUTTON GPIO_NUM_32
-// #define SLEEP_WAKEUP_STATE HIGH
-
-//setting low will colapse the power and the club will only turn on from USB or button press.
 
 // button pin
 #define BUTTON_COUNT 1
 #define BUTTON_INPUT_MODE INPUT
 const int buttonPins[BUTTON_COUNT]{ 39 };
 
-//battery sence
-// #define BATTERY_PIN 35 // takes the measurment from the battery sence. 
-
-// #define HAS_M5_IMU
-// #define HAS_IMU
-//#define I2C_CLOCK 10000 // not sure what this is for.
-// #define SDA_PIN 23 // i2c lines
-// #define SCL_PIN 22 // i2c lines
-// #define IMU_REMAP_CONFIG Adafruit_BNO055::REMAP_CONFIG_P0
-// #define IMU_REMAP_SIGN Adafruit_BNO055::REMAP_SIGN_P0
-
-// #define INT_PIN 33 // the interupt pin for the IMU MPU prosessor.
-// #define IMU_RESET 21 // toggl this pin to reset the IMU before connectiong to it after a reset.
-// #define IMU_ADDR 0x28
-// #define BNO055_SAMPLERATE_DELAY_MS (100)
-
-// ir config
-// #define HAS_IR
-
-// #define IR_TX_PIN 17 // drive with PWM HIGH is on and LOW is off.
-// #define IR_MAX_POWER 25 // 10%
-// #define IR_CHANNEL 0
-// #define IR_PWM_RESOLUTION 8 //8 = value range from 0 to 255
-// #define IR_FREQ 5000
-
-// SD config
-// #define HAS_FILES
-
-// #define SDSPEED 27000000
-
-// #define SD_MISO 19
-// #define SD_MOSI 23
-// #define SD_SCK 18
-// #define SD_CS 4
+#define HAS_M5_IMU
